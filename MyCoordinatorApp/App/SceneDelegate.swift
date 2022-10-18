26cf24089907331e6ffa2fdff6cacad642ef5d25
Mario <zukydeveloper@gmail.com>
2022-10-18 09:25:14 +0200

    code cleaning

--- MyCoordinatorApp/App/SceneDelegate.swift
+++ MyCoordinatorApp/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        coordinator = AppCoordinator(window: window!, scene: scene as? UIWindowScene)
+        coordinator = AppCoordinator(window: window, scene: scene as? UIWindowScene)
         coordinator?.start()
     }
 
@@ -47,7 +47,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
